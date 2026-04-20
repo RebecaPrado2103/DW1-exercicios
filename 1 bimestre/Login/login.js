6ed59f001d38ad0104773e43843f0f80ff47efe8
Rebeca Prado da Silva <rebecaprado@alunos.utfpr.edu.br>
2026-04-20 13:50:53 -0300

    alterações no menu.html

--- 1 bimestre/Login/login.js
+++ 1 bimestre/Login/login.js
@@ -6,7 +6,9 @@ function Entrar(){
     let mensagem = document.getElementById("mensagem");
 
     if(usuario === "admin" && senha === "123"){
-        window.open("menu.html", "_blank"); 
+        window.open("menu.html", "_blank");
+        footer.classList.remove("erro");
+        mensagem.innerHTML = "Login realizado com sucesso";
     } else {
         footer.classList.add("erro");
         mensagem.innerHTML = "Usuário ou senha incorretos";
